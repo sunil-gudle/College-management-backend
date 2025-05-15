f2cb3b5273990f5b33b22e829b533c7748e2899a
sunil-gudle <sunilgudle7844@gmail.com>
2025-05-15 22:18:27 +0530

    Initial commit

---
+++ src/test/java/com/dailycoding/restful_service/RestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycoding.restful_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
